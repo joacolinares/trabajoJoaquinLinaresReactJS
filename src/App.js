b69946534967ab8349885c6821567f07d0cf063c
joaquin linares <joacolinares2003@gmail.com>
2021-07-30 18:31:52 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar'
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <NavBar/>
+        
+        <p strong>Aceptamos todo tipo de pago</p>
+        
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
